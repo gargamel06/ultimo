bc3293b52849bd41437f14356d78bf3a9892767e
gargamel06 <160596859+gargamel06@users.noreply.github.com>
2024-11-05 08:11:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'JOGOS online',
+    'Qual é o impacto dos jogos de mundo aberto no desenvolvimento de narrativas interativas?',
+    ' os jogadores são transportados para o mundo virtual, podendo interagir, participar e transformar o espaço'
+)
+
+criaCartao(
+    'JOGOS online',
+    'Qual e o jogo mais jogado no mundo intero?',
+    'O jogo mais jogado e PlayerUnknown’s Battlegrounds.'
+)
+
+criaCartao(
+    'JOGOS online',
+    'Qual foi o papel da nostalgia no sucesso de remakes e remasterizações de jogos clássicos?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'volei',
+    'Quando que foi criado o volei?',
+    'foi criado em 1895 '
+)
+
+criaCartao(
+    'JOGOS online',
+    'qual e o primeiro jogo criado?',
+    'Por volta de 5.000 a.C a 4.000 a.C foram criados o Senet e o Jogo Real de Ur.'
+)
+criaCartao(
+    'JOGOS online',
+    'Qual é o nome do famoso vilão da série Metal Gear?',
+    ' O vilão mais famoso da série Metal Gear é Big Boss.'
+)
+criaCartao(
+    'JOGOS online',
+    'O que é um "open world" em termos de design de jogos?',
+    'é um tipo de design de jogo onde os jogadores podem explorar o ambiente de maneira não linear'
+)
+criaCartao(
+    'JOGOS online',
+    'Quem é o criador da franquia Super Mario?',
+    'Shigeru Miyamoto'
+)
+criaCartao(
+    'futebol',
+    'quantos times brasileiros já ganharam a libertadores',
+    '5 times '
+)
+criaCartao(
+    'volei',
+    'Quando que foi criado o volei?',
+    'foi criado em 1895 '
+)
+criaCartao(
+    'volei',
+    'Qual o objetivo do voleibol?',
+    'marcação de pontos ao mandar a bola para o campo adversário e fazer com que ela toque o chão.'
+)
